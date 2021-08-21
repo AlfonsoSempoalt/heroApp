f07fda7da591b76fe5d8ec150943175c36af84dd
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-08-20 23:19:29 -0600

    navBar heroApp

---
+++ src/components/DC/Dc.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dc = () => {
+    return (
+        <>
+            <h1>DC screen</h1>
+        </>
+    );
+};
+
+export default Dc;
---
+++ src/components/Marvel/Marvel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Marvel = () => {
+    return (
+        <>
+            <h1>Marvel Screen</h1>
+        </>
+    );
+};
+
+export default Marvel;
